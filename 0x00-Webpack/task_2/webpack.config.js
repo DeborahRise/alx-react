b84dc9a296ac0b322cd32c0caf57849c5ea0fd10
DeborahRise <debie.rise.bookings@gmail.com>
2024-07-23 09:23:49 +0100

    task2

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -6,5 +6,30 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'public')
     },
-    mode: 'production'
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[hash].[ext]',
+                            outputPath: 'assets'
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    mode: 'production',
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+        },
+    },
 };
