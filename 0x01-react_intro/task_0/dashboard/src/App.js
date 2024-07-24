e00f629c839d50a57f569a8e411c05263ed864c2
DeborahRise <debie.rise.bookings@gmail.com>
2024-07-24 16:58:46 +0100

    app.css included

--- 0x01-react_intro/task_0/dashboard/src/App.js
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -3,30 +3,27 @@ import './App.css';
 
 function App() {
   return (
-    <>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="Holberton Logo" />
-        <h1>
+        <img src={logo} alt="Holberton Logo" />
+        <h1 className="heading1">
         School dashboard
         </h1>
       </header>
-    </div>
-    <div>
+  
+      
       <body className="App-body" >
         <p>
         Login to access the full dashboard
         </p>
       </body>
-    </div>
-    <div>
+      
       <footer className="App-footer" >
         <p>
         Copyright 2020 - holberton School
         </p>
       </footer>
     </div>
-    </>
   );
 }
 
