b6486cf3ae1d30945234c60ba123db39db985414
DeborahRise <debie.rise.bookings@gmail.com>
2024-07-24 18:59:21 +0100

    task 2 part 1 done

--- 0x01-react_intro/task_1/dashboards/src/App.js
+++ 0x01-react_intro/task_1/dashboards/src/App.js
@@ -1,5 +1,6 @@
 import logo from './holberton-logo.png';
 import './App.css';
+import { getFullYear, getFooterCopy } from './utils'
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
         <p>Login to access the full dashboard</p>
       </body>
       <footer className='App-footer'>
-        <p>Copyright 2020 - holberton School</p>
+        <p>Copyright {getFullYear()} - {getFooterCopy(true)} </p>
       </footer>
     </div>
   );
