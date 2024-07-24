526989df1f0a44f5dfec3af8487ed613e520a87a
DeborahRise <debie.rise.bookings@gmail.com>
2024-07-25 00:52:55 +0100

    enzyme

---
+++ 0x01-react_intro/task_3/dashboards/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import Notifications from './Notifications';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+    
+  </React.StrictMode>
+);
+const root_notifications = ReactDOM.createRoot(document.getElementById('root-notifications'));
+root_notifications.render(
+  <React.StrictMode>
+    <Notifications />
+  </React.StrictMode>
+)
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
