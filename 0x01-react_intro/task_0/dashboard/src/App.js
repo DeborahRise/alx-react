d31f0d120f93e758e1c1ba1b9db1362c54f04b7e
DeborahRise <debie.rise.bookings@gmail.com>
2024-07-24 16:37:10 +0100

    comiled succesfully

--- 0x01-react_intro/task_0/dashboard/src/App.js
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import logo from './holberton-logo.png';
 import './App.css';
 
 function App() {
   return (
+    <>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="Holberton Logo" />
+        <h1>
+        School dashboard
+        </h1>
       </header>
     </div>
+    <div>
+      <body className="App-body" >
+        <p>
+        Login to access the full dashboard
+        </p>
+      </body>
+    </div>
+    <div>
+      <footer className="App-footer" >
+        <p>
+        Copyright 2020 - holberton School
+        </p>
+      </footer>
+    </div>
+    </>
   );
 }
 
