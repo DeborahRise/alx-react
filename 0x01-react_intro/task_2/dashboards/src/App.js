788a326b60cd864c688d8873984b10ba81b8990f
DeborahRise <debie.rise.bookings@gmail.com>
2024-07-24 19:53:54 +0100

    done with task 3 by adding labels and inputs for email and password

---
+++ 0x01-react_intro/task_2/dashboards/src/App.js
@@ -0,0 +1,29 @@
+import logo from './holberton-logo.png';
+import './App.css';
+import { getFullYear, getFooterCopy } from './utils'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} alt="logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <body className='App-body'>
+        <p>Login to access the full dashboard</p>
+        <label>Email</label>
+        <input type="text">
+        </input>
+        <label>Password</label>
+        <input type="password">
+        </input>
+        <button>OK</button>
+      </body>
+      <footer className='App-footer'>
+        <p>Copyright {getFullYear()} - {getFooterCopy(true)} </p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ 0x01-react_intro/task_2/dashboards/src/utils.js
@@ -0,0 +1,12 @@
+export function getFullYear () {
+    return (
+        new Date().getFullYear()
+    )
+}
+
+export function getFooterCopy ( {isIndex} ) {
+    return (
+        isIndex ? 'Holberton School' : 'Holberton School main dashboard'
+    )
+
+}
