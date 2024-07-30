d969092ddefa7c3a1b69d2bd08da8d2ee405094b
DeborahRise <debie.rise.bookings@gmail.com>
2024-07-30 01:01:34 +0100

    modules/

---
+++ 0x00-Webpack/task_3/modules/header/header.js
@@ -0,0 +1,8 @@
+import $ from 'jquery';
+import './header.css';
+
+$(document).ready(function() {
+    $('header').append('<img src="logo.png" alt="Holberton Logo">');
+    $('header').append('<h1>Holberton Dashboard</h1>');
+    console.log('Init header');
+  });
