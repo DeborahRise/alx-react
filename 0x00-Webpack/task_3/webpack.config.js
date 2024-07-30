b0fbc5d631c1501c520ea3944dafaa802fdd13d8
DeborahRise <debie.rise.bookings@gmail.com>
2024-07-30 13:52:38 +0100

    webpack.config.js

--- 0x00-Webpack/task_3/webpack.config.js
+++ 0x00-Webpack/task_3/webpack.config.js
@@ -1,9 +1,12 @@
+const { template, chunk } = require("lodash");
 const path = require("path");
+const { splitChunks } = require("webpack");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: "development",
   entry: {
-    main: path.resolve(__dirname, "./js/dashboard_main.js"),
     header: path.resolve(__dirname, './header/header.js'),
     body: path.resolve(__dirname, './body/body.js'),
     footer: path.resolve(__dirname, './footer/footer.js')
@@ -37,5 +40,17 @@ module.exports = {
     contentBase: path.resolve(__dirname, "public"),
     port: 8564,
     open: true,
+  },
+  devtools: 'inline-source-map', //this line enables inline source maps
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'index.html'),
+    }),
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: 'all', //this enables code splitting for all chunks
+    },
   },
 };
