742651584cf6002089f02608a70799bf01e7afc1
DeborahRise <debie.rise.bookings@gmail.com>
2024-08-05 05:08:12 +0100

    tests for NotificationItem and main component

--- 0x02-react_props/task_2/dashboard/src/Notifications/Notifications.test.js
+++ 0x02-react_props/task_2/dashboard/src/Notifications/Notifications.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { shallow } from "enzyme";
 import Notifications from "./Notifications";
+import NotificationItem from "./NotificationItem";
 
 describe("Notification component tests", () => {
   it("renders Notification component without crashing", () => {
@@ -18,12 +19,18 @@ describe("Notification component tests", () => {
   it("renders three list items", () => {
     const notification = shallow(<Notifications />);
 
-    expect(notification.find("li")).toHaveLength(3);
+    expect(notification.find(NotificationItem)).toHaveLength(3);
   });
 
   it("renders correct text", () => {
     const notification = shallow(<Notifications />);
 
     expect(notification.find("p").text()).toBe("Here is the list of notifications");
+  });
+  it("renders first NotificationItem with correct html", () => {
+    const notification = shallow(<Notifications />);
+    const firstNotificationItem = notification.find(NotificationItem).at(0);
+    expect(firstNotificationItem.html()).toContain("data-notification-type=\"default\"");
+    expect(firstNotificationItem.html()).toContain("New course available");
   });
 });
