d1fe63335cbd60256bfe42396d700011b41a7126
DeborahRise <debie.rise.bookings@gmail.com>
2024-08-05 19:13:10 +0100

    modified the App.js

--- 0x02-react_props/task_4/dashboard/src/App/App.js
+++ 0x02-react_props/task_4/dashboard/src/App/App.js
@@ -1,21 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './App.css';
 import Login from '../Login/Login';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import Notifications from '../Notifications';
+import CourseList from '../CourseList/CourseList'
 
-function App() {
+function App({ isLoggedIn}) {
   return (
     <>
     <Notifications />
     <div className="App">
       <Header />
-      <Login />
+      {isLoggedIn ? <CourseList/> : <Login />}
       <Footer />
     </div>
     </>
   );
 }
 
+App.PropTypes = {
+  isLoggedIn: PropTypes.bool,
+};
+
+App.defaultProps = {
+  isLoggedIn: false
+};
 export default App;
