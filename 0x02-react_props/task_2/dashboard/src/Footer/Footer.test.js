52500fbc08f94e160d98231c874cc78f992c950f
DeborahRise <debie.rise.bookings@gmail.com>
2024-08-05 03:30:34 +0100

    test: Update Footer.test.js with new test cases

--- 0x02-react_props/task_2/dashboard/src/Footer/Footer.test.js
+++ 0x02-react_props/task_2/dashboard/src/Footer/Footer.test.js
@@ -1,3 +1,16 @@
+import React from 'react';
 import Footer from './Footer';
 import { shallow } from 'enzyme';
+import { getFullYear, getFooterCopy } from '../utils';
 
+
+describe("Footer test", () => {
+    it("should render without crashing", () => {
+      const wrapper = shallow(<Footer />);
+      expect(wrapper.exists()).toEqual(true);
+    });
+    it("should render the text Copyright", () => {
+      const wrapper = shallow(<Footer />);
+      expect(wrapper.text()).toEqual(`Copyright ${getFullYear()} - ${getFooterCopy()}`);
+    });
+  });
