f2a7db2abe3278d92d7f7943f037b818735b4563
DeborahRise <debie.rise.bookings@gmail.com>
2024-08-06 11:48:04 +0100

    Modify App.js to add listCourses and listNotifications

--- 0x02-react_props/task_5/dashboard/src/App/App.js
+++ 0x02-react_props/task_5/dashboard/src/App/App.js
@@ -7,13 +7,38 @@ import Footer from '../Footer/Footer';
 import Notifications from '../Notifications';
 import CourseList from '../CourseList/CourseList'
 
+  const listCourses = [
+    {
+      id: 1,
+      name: 'ES6',
+      credit: 60
+    },
+    {
+      id: 2,
+      name: 'Webpack',
+      credit: 20
+    },
+    {
+      id: 3,
+      name: 'React',
+      credit: 40
+    }
+  ];
+
+  const listNotifications = [
+    { id: 1, type: 'default', value: 'New course available' },
+    { id: 2, type: 'urgent', value: 'New resume available' },
+    { id: 3, type: 'urgent', html: { __html: getLatestNotification() } }
+  ];
+
 function App({ isLoggedIn}) {
+
   return (
     <>
-    <Notifications />
+    <Notifications displayDrawer={false} listNotifications={listNotifications} />
     <div className="App">
       <Header />
-      {isLoggedIn ? <CourseList/> : <Login />}
+      {isLoggedIn ? <CourseList listCourses={listCourses}/> : <Login />}
       <Footer />
     </div>
     </>
