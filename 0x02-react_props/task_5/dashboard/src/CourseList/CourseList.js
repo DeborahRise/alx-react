d2796a4debbdfd74c0e95e7e5111465528c8f3d7
DeborahRise <debie.rise.bookings@gmail.com>
2024-08-06 11:48:04 +0100

    Update CourseList.js to handle listCourses prop and display appropriate messages or rows

--- 0x02-react_props/task_5/dashboard/src/CourseList/CourseList.js
+++ 0x02-react_props/task_5/dashboard/src/CourseList/CourseList.js
@@ -1,21 +1,36 @@
 import React from 'react'
 import CourseListRow from './CourseListRow'
+import PropTypes from 'prop-types';
 import './CourseList.css'
+import CourseShape from './CourseShape';
 
-const CourseList = () => {
+const CourseList = ({ listCourses }) => {
   return (
     <table id='CourseList'>
-        <thead>
-            <CourseListRow isHeader={true} textFirstCell="Available courses" />
-            <CourseListRow isHeader={true} textFirstCell="Course name" textSecondCell="Credit" />
-        </thead>
-        <tbody>
-            <CourseListRow isHeader={false} textFirstCell="ES6" textSecondCell="60" />
-            <CourseListRow isHeader={false} textFirstCell="Webpack" textSecondCell="20" />
-            <CourseListRow isHeader={false} textFirstCell="React" textSecondCell="40" />
-        </tbody>
-    </table>
-  )
-}
+       <thead>
+           <CourseListRow isHeader={true} textFirstCell="Available courses" />
+           <CourseListRow isHeader={true} textFirstCell="Course name" textSecondCell="Credit" />
+       </thead>
+       <tbody>
+        {!listCourses ? (
+          <CourseListRow isHeader={false} textFirstCell="No course available yet" textSecondCell="" />
+        ) : (
+          listCourses.map(course => (
+            <CourseListRow key={course.id} isHeader={false} textFirstCell={course.name} textSecondCell={course.credit} />
+          ))
+        )
+           }
+       </tbody>
+   </table>
+  );
+};
 
-export default CourseList
+CourseList.propTypes = {
+  listCourses: PropTypes.arrayOf(CourseShape)
+};
+
+CourseList.defaultProps = {
+  listCourses: []
+};
+
+export default CourseList;
