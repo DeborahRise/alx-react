1a82152823a206853371d15f270746bd7bb9e8cc
DeborahRise <debie.rise.bookings@gmail.com>
2024-08-06 13:29:42 +0100

    all

--- 0x02-react_props/task_5/dashboard/src/CourseList/CourseList.test.js
+++ 0x02-react_props/task_5/dashboard/src/CourseList/CourseList.test.js
@@ -3,6 +3,12 @@ import CourseList from "./CourseList";
 import CourseListRow from "./CourseListRow";
 import { shallow } from "enzyme";
 
+const listCourses = [
+  { id: 1, name: "ES6", credit: 60 },
+  { id: 2, name: "Webpack", credit: 20 },
+  { id: 3, name: "React", credit: 40 },
+];
+
 describe('CourseList Component Test', () => {
     it('renders without crashing', () => {
         const component = shallow(<CourseList />);
@@ -39,5 +45,14 @@ describe('CourseList Component Test', () => {
             textFirstCell: "React",
             textSecondCell: "40",
           });
+    });
+
+    it("renders correctely when passed a list of courses", () => {
+      const wrapper = shallow(<CourseList listCourses={listCourses} />);
+  
+      expect(wrapper.find("tbody").children()).toHaveLength(3);
+      expect(wrapper.find("tbody").childAt(0).html()).toEqual("<tr><td>ES6</td><td>60</td></tr>");
+      expect(wrapper.find("tbody").childAt(1).html()).toEqual("<tr><td>Webpack</td><td>20</td></tr>");
+      expect(wrapper.find("tbody").childAt(2).html()).toEqual("<tr><td>React</td><td>40</td></tr>");
     });
 })
