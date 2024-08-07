a53f5ae5d32312cb563dd46ac661e36bba1cea66
DeborahRise <debie.rise.bookings@gmail.com>
2024-08-07 14:03:24 +0100

    task 3

---
+++ 0x03-React_component/task_3/dashboard/src/App/App.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import PropTypes, { func } from 'prop-types';
+import './App.css';
+import Login from '../Login/Login';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import Notifications from '../Notifications/Notifications';
+import CourseList from '../CourseList/CourseList'
+import { getLatestNotification } from '../utils';
+
+  const listCourses = [
+    {
+      id: 1,
+      name: 'ES6',
+      credit: 60
+    },
+    {
+      id: 2,
+      name: 'Webpack',
+      credit: 20
+    },
+    {
+      id: 3,
+      name: 'React',
+      credit: 40
+    }
+  ];
+
+  const listNotifications = [
+    { id: 1, type: 'default', value: 'New course available' },
+    { id: 2, type: 'urgent', value: 'New resume available' },
+    { id: 3, type: 'urgent', html: { __html: getLatestNotification() } }
+  ];
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown(event) {
+    const { logOut } = this.props;
+    if (event.ctrlKey && event.key=== 'h') {
+      alert('Logging you Out');
+      logOut();
+    }
+  }
+  render() {
+
+  const { isLoggedIn, logOut } = this.props;
+  return (
+    <>
+    <Notifications displayDrawer={true} listNotifications={listNotifications} />
+    <div className="App">
+      <Header />
+      {isLoggedIn ? <CourseList listCourses={listCourses}/> : <Login />}
+      <Footer />
+    </div>
+    </>
+  );
+}
+}
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+  logOut: PropTypes.func,
+};
+
+App.defaultProps = {
+  isLoggedIn: false,
+  logOut: () => {},
+};
+export default App;
---
+++ 0x03-React_component/task_3/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const NotificationItem = ({ type, html, value, markAsRead, id }) => {
+  if (html) {
+    return <li data-notification-type={type} dangerouslySetInnerHTML={{ __html: html }} onClick={() => markAsRead(id)}></li>;
+  }
+  return <li data-notification-type={type} onClick={() => markAsRead(id)}>{value}</li>;
+};
+
+NotificationItem.propTypes = {
+  html: PropTypes.shape({
+    __html: PropTypes.string.isRequired,
+  }),
+  value: PropTypes.string,
+  type: PropTypes.string.isRequired,
+  markAsRead: PropTypes.func.isRequired,
+  id: PropTypes.number.isRequired
+};
+
+NotificationItem.defaultProps = {
+  type: 'default',
+  value: '',
+};
+
+export default NotificationItem;
