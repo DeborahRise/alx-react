f235b6c81d6484548190fe8c6b98f1714dd6f8bb
DeborahRise <debie.rise.bookings@gmail.com>
2024-08-08 01:57:32 +0100

    dashboard/src/BodySection// Body

--- 0x03-React_component/task_3/dashboard/src/BodySection/BodySection.test.js
+++ 0x03-React_component/task_3/dashboard/src/BodySection/BodySection.test.js
@@ -4,10 +4,16 @@ import { shallow } from "enzyme";
 
 describe('BodySection Component Test', () => {
     it('renders without crashing', () => {
-        const wrapper = shallow(<BodySection title={title} children={children} />);
+        const wrapper = shallow(<BodySection title="test title"> children="<p>test children node</p>" </BodySection> );
 
         expect(wrapper.exists()).toBe(true);
-        expect(wrapper.contains("h2").find("test title")).toBe(true);
-        expect(wrapper.find("h2").html).toEqual("test title");
     });
+    it('', () => {
+        const wrapper = shallow();
+
+        expect(wrapper.find("h2")).lenght(1);
+        expect(wrapper.find("h2").text()).toBe("test title");
+        expect(wrapper.find("p").text()).toBe("test children node");
+    
+    })
 });
