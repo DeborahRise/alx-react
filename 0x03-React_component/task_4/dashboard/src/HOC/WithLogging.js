5ffc1572c5a59a49f7af454415f46835c2b70388
DeborahRise <debie.rise.bookings@gmail.com>
2024-08-08 12:06:47 +0100

    dashboard/src/HOC/WithLogging.js

---
+++ 0x03-React_component/task_4/dashboard/src/HOC/WithLogging.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+const WithLogging = (WrappedComponent) => {
+  return class extends Component {
+    componentDidMount() {
+      const componentName = WrappedComponent.displayName || WrappedComponent.name || 'Component';
+      console.log(`Component ${componentName} is mounted`);
+    }
+
+    componentWillUnmount() {
+      const componentName = WrappedComponent.displayName || WrappedComponent.name || 'Component';
+      console.log(`Component ${componentName} is going to unmount`);
+    }
+
+    render() {
+      return <WrappedComponent {...this.props} />;
+    }
+  };
+};
+
+// Set displayName for easier debugging and React DevTools integration
+WithLogging.displayName = `WithLogging(${WrappedComponent.displayName || WrappedComponent.name || 'Component'})`;
+
+export default WithLogging;
