1cc6ad6dfd0ae24c385b210f72867a8a38b9869f
DeborahRise <debie.rise.bookings@gmail.com>
2024-08-09 12:24:17 +0100

    added back mistakenly deleted files

--- 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseList.js
+++ 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseList.js
@@ -1,26 +1,12 @@
 import React from 'react'
 import CourseListRow from './CourseListRow'
 import PropTypes from 'prop-types';
-import { StyleSheet, css } from 'aphrodite';
+import './CourseList.css'
 import CourseShape from './CourseShape';
-
-const styles = StyleSheet.create({
-  table: {
-    width: '100%',
-    border: '1px solid #ccc',
-  },
-  th: {
-    backgroundColor: '#f5f5f5',
-    textAlign: 'left',
-  },
-  td: {
-    padding: '8px',
-  },
-});
 
 const CourseList = ({ listCourses }) => {
   return (
-    <table className={css(styles.table)}>
+    <table id='CourseList'>
        <thead>
            <CourseListRow isHeader={true} textFirstCell="Available courses" />
            <CourseListRow isHeader={true} textFirstCell="Course name" textSecondCell="Credit" />
--- 0x04-React_inline_styling/task_1/dashboard/src/CourseList/CourseList.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/CourseList/CourseList.js
@@ -1,13 +1,27 @@
 import React from 'react'
 import CourseListRow from './CourseListRow'
 import PropTypes from 'prop-types';
-import './CourseList.css'
+import { StyleSheet, css } from 'aphrodite';
 import CourseShape from './CourseShape';
+
+const styles = StyleSheet.create({
+  table: {
+    width: '100%',
+    border: '2px solid #ccc',
+  },
+  th: {
+    backgroundColor: '#deb5b5',
+    textAlign: 'center',
+  },
+  td: {
+    padding: '8px',
+  },
+});
 
 const CourseList = ({ listCourses }) => {
   return (
-    <table id='CourseList'>
-       <thead>
+    <table id='CourseList' className={css(styles.table)}>
+       <thead className={css(styles.th)}> 
            <CourseListRow isHeader={true} textFirstCell="Available courses" />
            <CourseListRow isHeader={true} textFirstCell="Course name" textSecondCell="Credit" />
        </thead>
