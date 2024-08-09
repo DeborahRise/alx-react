f3e33c38254ce40e5e761dc023d88c27dd29b295
DeborahRise <debie.rise.bookings@gmail.com>
2024-08-09 13:20:17 +0100

    login styling

--- 0x04-React_inline_styling/task_1/dashboard/src/Login/Login.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/Login/Login.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import './Login.css';
+import { StyleSheet, css } from 'aphrodite';
+
+const styles = StyleSheet.create({
+  AppBody: {
+    height: '50vh',
+    borderBottom: '4px solid rgb(220, 34, 34)',
+  },
+});
 
 const Login = () => {
   return (
     <div>
-        <div className='App-body'>
+        <div className={css(styles.AppBody)}>
         <p>Login to access the full dashboard</p>
         <label>Email:</label>
         <input type="text" />
