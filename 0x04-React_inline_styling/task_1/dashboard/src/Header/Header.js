c484476ec99b2686f604bfd017221ca0078f1384
DeborahRise <debie.rise.bookings@gmail.com>
2024-08-09 13:10:43 +0100

    header styling

--- 0x04-React_inline_styling/task_1/dashboard/src/Header/Header.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/Header/Header.js
@@ -1,13 +1,25 @@
 import React from 'react';
-import './Header.css';
-
+import {StyleSheet, css} from 'aphrodite'
 import logo from '../holberton-logo.png';
+
+const styles = StyleSheet.create({
+  AppHeader: {
+    display: 'flex',
+    boaderBottom: '4px solid rgb(220, 34, 34)',
+    color: 'rgb(230, 34, 34)',
+    alignItems: 'center',
+    marginTop: '10rem',
+  },
+  AppHeaderImg: {
+    height: '150px',
+  }
+})
 
 const Header = () => {
   return (
     <div>
-        <header className="App-header">
-        <img src={logo} alt="logo" />
+        <header className={css(styles.AppHeader)}>
+        <img className={css(styles.AppHeaderImg)} src={logo} alt="logo" />
         <h1>School dashboard</h1>
       </header>
     </div>
