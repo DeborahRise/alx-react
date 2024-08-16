1620481c4e6b4149f07efbfbe9dd491566d2a43c
DeborahRise <debie.rise.bookings@gmail.com>
2024-08-16 04:18:52 +0100

    task 5

---
+++ 0x06-React_Immutable/5-merge.js
@@ -0,0 +1,9 @@
+import { Map, List } from 'immutable';
+
+export const concatElements = (page1, page2) => {
+    return List(page1).concat(List(page2));
+}
+
+export const mergeElements = (page1, page2) => {
+    return Map(page1).merge(Map(page2));
+}
