87948b7b6ae210cfe06aadcca929e2d25b368b7d
DeborahRise <debie.rise.bookings@gmail.com>
2024-08-16 03:36:37 +0100

    no need for example

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -6,15 +6,3 @@ function getImmutableObject(object) {
     return fromJS(object);
 }
 
-// Example usage
-const myObject = {
-    name: "John",
-    age: 30,
-    address: {
-        city: "New York",
-        zip: "10001"
-    }
-};
-
-const immutableMap = getImmutableObject(myObject);
-console.log(immutableMap);
